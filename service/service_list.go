b6d23d5cb736aba40c9b26d741f02ab2bcc10ea2
nurfianqodar <77nurfianqodar@gmail.com>
2024-12-23 10:42:37 +0700

    fix user list method

--- service/service_list.go
+++ service/service_list.go
@@ -14,22 +14,32 @@ func (s *server) UserList(
 	req *user_proto.InUserList,
 ) (*user_proto.OutUserList, error) {
 	var users []db.User
+	query := s.db.Model(&db.User{})
+
+	// Apply pagination if request specifies it
 	if req != nil {
-		if err := s.db.Limit(int(req.Take)).Offset(int(req.Start)).Find(&users); err != nil {
-			log.Println(err.Error)
-			return nil, errors.InternalError
+		if req.Take > 0 {
+			query = query.Limit(int(req.Take))
 		}
-	} else {
-		if err := s.db.Find(&users); err != nil {
-			log.Println(err.Error)
-			return nil, errors.InternalError
+		if req.Start > 0 {
+			query = query.Offset(int(req.Start))
 		}
 	}
+
+	// Execute query
+	if err := query.Find(&users).Error; err != nil {
+		log.Printf("Error fetching users: %v\n", err)
+		return nil, errors.InternalError
+	}
+
+	// Transform users to proto format
 	resUsers := make([]*user_proto.OutUserDetail, len(users))
 	for i, u := range users {
-		resUsers[i].Id = u.Id
-		resUsers[i].Username = u.Username
-		resUsers[i].FullName = u.FullName
+		resUsers[i] = &user_proto.OutUserDetail{
+			Id:       u.Id,
+			Username: u.Username,
+			FullName: u.FullName,
+		}
 	}
 
 	return &user_proto.OutUserList{Users: resUsers}, nil
